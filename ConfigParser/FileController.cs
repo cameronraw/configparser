9e014cf54e7dd0a26023862cf219f354a3c9025c
cameronraw <cameron.raw89@gmail.com>
2022-09-05 21:31:25 +0200

    Achieves grouping of files ending in Config.txt via FileController

---
+++ ConfigParser/FileController.cs
@@ -0,0 +1,20 @@
+﻿namespace ConfigParser;
+
+public class FileController
+{
+    private readonly string _filePath;
+
+    public FileController(string filePath = "./TextFiles")
+    {
+        _filePath = filePath;
+    }
+
+    public IEnumerable<string> GetConfigFiles()
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var directoryPrefix = Directory.GetParent(currentDirectory)!.Parent!.Parent!.FullName;
+        
+        var allFiles = Directory.GetFiles($"{directoryPrefix}/{_filePath}");
+        return allFiles.Where(c => c.Contains("Config.txt"));
+    }
+}
---
+++ UnitTests/FileControllerShould.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+
+namespace ConfigParser;
+
+public class FileControllerShould
+{
+    [Test]
+    public void DetectFiles_WithConfigInTheName()
+    {
+        var fileController = new FileController("TestTextFiles");
+        IEnumerable<string> configFiles = fileController.GetConfigFiles();
+
+        configFiles.Should().HaveCount(1);
+    }
+}
